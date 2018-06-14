779fa3bc1afb55df4bdfea3aed30334fb26708f8
Chris_luo <954926501@qq.com>
2018-06-14 10:32:23 +0800

    梯度爆炸

---
+++ Bayesian/Bayesian.py
@@ -0,0 +1,16 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+'''
+梯度爆炸
+'''
+dimensionality = 10
+print(1 / (0.01 ** dimensionality))
+
+max_dimensionality = 10
+ax = plt.axes(xlim=(0, max_dimensionality), ylim=(0, 1 / (0.01 ** max_dimensionality)))
+x = np.linspace(0, max_dimensionality, 1000)
+y = 1 / (0.01 ** x)
+plt.plot(x, y, lw=2)
+plt.show()
